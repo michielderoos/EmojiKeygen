5f52eebcb0ca8a02066331247be500a0b737b254
Michiel <michiel@deroos.ca>
2019-11-28 15:23:17 -0400

    Add variable strategies

--- emojikeygen/resources/shorten.py
+++ emojikeygen/resources/shorten.py
@@ -1,10 +1,11 @@
 from flask_restful import Resource, reqparse, abort
 from sqlalchemy.sql.expression import func
-import urllib
 
-from emojikeygen.shorteners import emojihash
-from emojikeygen.shorteners import util
+from emojikeygen.shorteners import emojihash, shortseq
 from emojikeygen.models import keys, db
+
+# Strategies we want to make available through the API
+strategies = {'emojihash': emojihash, 'shortseq': shortseq}
 
 class Shorten(Resource):
     def get(self, emojikey):
@@ -13,15 +14,20 @@ class Shorten(Resource):
         if not key_row:
             abort(404, message="Emojikey {} does not exist".format(key_row))
         return {'name': key_row.name}
+
     def post(self):
         # Gather all the required data to generate and store a new emojikey
         parser = reqparse.RequestParser()
         parser.add_argument('name', required = True, type=str, help='Name of user asking for a token')
         parser.add_argument('strategy', default = 'emojihash',type=str, help='Shortening strategy to use')
         args = parser.parse_args()
+        # Get current index and desired strategy
         emojikey_index = db.session.query(func.max(keys.id)).scalar() or 0
+        if args.strategy not in strategies:
+            abort(400, message="Invalid Strategy: Strategy {} does not exist".format(args.strategy))
+        strategy = strategies[args.strategy]
         # Generate and store emojikey
-        emojikey, key = emojihash.generate(emojikey_index)
+        emojikey, key = strategy.generate(emojikey_index)
         key_row = keys(emojikey = emojikey, name = args.name, key = key, strategy = args.strategy)
         db.session.add(key_row)
         db.session.commit()
--- emojikeygen/shorteners/shortseq.py
+++ emojikeygen/shorteners/shortseq.py
@@ -1,4 +1,4 @@
-#emojihash shortening method (Documented in README.md - Strategy 1)
+#shortseq shortening method (Documented in README.md - Strategy 2)
 import hashlib
 
 from emojikeygen import config
@@ -6,12 +6,10 @@ from emojikeygen import config
 from emojikeygen.shorteners import alphabet
 from emojikeygen.shorteners import util
 
-def generate(index, random_sequence_length = config.DEFAULT_RANDOM_SEQUENCE_LENGTH):
+def generate(index):
     """Generates emoji sequence and private key"""
-    index_sequence = util.int_to_list(index, shorten_base = True)
-    random_sequence = util.generate_random_list(random_sequence_length)
-    emojikey = util.list_to_emoji_string(index_sequence) + alphabet.sentinel + util.list_to_emoji_string(random_sequence)
-    return emojikey, generate_key(emojikey)
+    index_sequence = util.list_to_emoji_string(util.int_to_list(index, shorten_base = False))
+    return index_sequence, generate_key(index_sequence)
 
 def generate_key(emojikey):
     """Generates key when given emojikey. Used by generate function, but can also be used to decode any given emojikey"""
@@ -19,4 +17,3 @@ def generate_key(emojikey):
     hash = hashlib.sha256()
     hash.update(peppered_emojikey.encode('utf-8'))
     return hash.hexdigest()
-
