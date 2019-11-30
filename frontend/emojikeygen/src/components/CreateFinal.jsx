47bdb760efa4ee6055745245f670bfbb4b1ea401
Michiel <michiel@deroos.ca>
2019-11-30 11:25:00 -0400

    Make create-final more readable

--- frontend/emojikeygen/src/components/CreateFinal.jsx
+++ frontend/emojikeygen/src/components/CreateFinal.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class create extends Component{
     render(){
         return(
-            <h1 className="ui centered">Your emojikey is {this.props.values.emojikey}</h1>
+            <h1 className="ui centered">Your emojikey is <br/>{this.props.values.emojikey}</h1>
         )
     }
 }
