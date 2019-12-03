18d2b886c8d38e1e065c26eef892822d9ee274fd
Michiel <michiel@deroos.ca>
2019-12-02 22:50:40 -0400

    Mess around with markov encoding

--- emojikeygen/shorteners/markovEncode.py
+++ emojikeygen/shorteners/markovEncode.py
@@ -35,23 +35,34 @@ def accumulate(iterable, func=operator.add):
 		total = func(total, element)
 		yield total
 
-def convert_variable_base(key, input_base, output_base):
+def convert_variable_base2(key, input_base, output_base):
 	bc = baseconvert.BaseConverter(input_base=input_base, output_base=output_base)
 	converted = list(bc(key))
 	return converted
+
+def convert_variable_base(key, input_base, output_base):
+	#print(convert_variable_base2(key, input_base, 16))
+	bc = baseconvert.BaseConverter(input_base=input_base, output_base=output_base)
+	converted = list(bc(key))
+	return converted
+
+def generate_choices(state, chain, ):
+	choices, weights = zip(*chain.model[state].items())
+	return choices, weights
 
 def move(chain, state, key, key_base):
 	if state == tuple([ BEGIN ] * chain.state_size):
 		choices = chain.begin_choices
 		cumdist = chain.begin_cumdist
 	else:
-		choices, weights = zip(*chain.model[state].items())
+		choices, weights = generate_choices(state, chain)
 		cumdist = list(accumulate(weights))
 
 	if len(choices) > 1:
 		if key:
 			base_n_key = convert_variable_base(key, key_base, len(choices))
-			return choices[base_n_key[0]], base_n_key, len(choices)
+			print(base_n_key[-1], len(choices))
+			return choices[base_n_key[-1]], base_n_key, len(choices)
 		else:
 			r = random.random() * cumdist[-1]
 			return choices[bisect.bisect(cumdist, r)], None, None
@@ -61,20 +72,18 @@ def move(chain, state, key, key_base):
 
 def gen(key, chain, key_base = 16):
 	state = (BEGIN,) * chain.state_size
-	encoded_key_ints = []
 	complete = False
 	sentences = []
 	sentence = []
 	while True:
 		next_word, key, key_base = move(chain, state, key, key_base)
-		if key:
-			encoded_key_ints.append(key.pop(0))
-		else:
+		if not key:
 			complete = True
-
+		if key and key_base > 1:
+			key.pop()
 		if next_word == END and complete: 
 			sentences += sentence
-			return sentences
+			return ' '.join(sentences)
 		if next_word == END and not complete:
 			sentences += sentence
 			sentence = []
@@ -82,46 +91,40 @@ def gen(key, chain, key_base = 16):
 		else:
 			sentence.append(next_word)
 			state = tuple(state[1:]) + (next_word,)
-	return sentences
+	return ' '.join(sentences)
 
 
-def decodeStep(state, chain, ):
-	choices, weights = zip(*chain.model[state].items())
-	return choices
+def decode(paragraph, chain):
+	numbers = []
+	bases = []
+	paragraph = markovify.split_into_sentences(phrase)
+	for sentence in paragraph:
+		sentence = sentence.split(' ')
+		firstWord = sentence.pop(0)
+		firstIndex = chain.begin_choices.index(firstWord)
+		numbers.append(firstIndex)
+		bases.append(len(chain.begin_choices))
+		state = (BEGIN, firstWord)
+		for word in sentence:
+			choices, weights = generate_choices(state, chain)
+			state = (state[1], word)
+			if len(choices) <= 1:
+				continue
+			numbers.append(choices.index(word))
+			bases.append(len(choices))
+	return zip(numbers, bases)
 
-def decode(key, chain):
-	newSentence = True
-	for word in key:
-		if newSentence:
-			newSentence = False
-			firstIndex = chain.begin_choices.index(word)
-			state = (BEGIN, word)
-			continue
-		choices = decodeStep(state, chain)
-		state = (state[1], word)
-		print(word)
-		if markovify.splitters.is_sentence_ender(word):
-			print(word)
-			print('ENDER!')
-			newSentence = True
-		if len(choices) <= 1:
-			continue
-		print(choices.index(word))
-
-	#for word in key:
-	#	state = (state[1], word)
-	#	try:
-	#		choices = decodeStep(state, chain)
-	#		allChoices.append(allChoices)
-	#	except:
-	#		print('ALL CHOICES!')
-	#		for line in allChoices:
-	#			print(line)
-word_to_encode = '130b3d5871767881bf93a0fdb5cc9e14130b3d5871767881bf93a0fdb5cc9e14'.upper()
+word_to_encode = '080464c7ea55dc3ef9b442dc888d18f5080464c7ea55dc3ef9b442dc888d18f5'.upper()
 phrase = gen(word_to_encode, text_model.chain)
-print(phrase)
+print(markovify.split_into_sentences(phrase))
 print('AAAZZZZZZZZZ-------------------------')
-print(decode(phrase, text_model.chain))
+count = 0
+for number, base in decode(phrase, text_model.chain):
+	print(number, base)
+	#print(convert_variable_base2(number, base, 16))
+	#num = convert_variable_base(number, base, 10)
+	#num = int(''.join([str(i) for i in num]))
+	#count += num
 print('AAAZZZZZZZZZ-------------------------')
 
 
